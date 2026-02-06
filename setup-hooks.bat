8cef52d6ecd44d259f268a1dd5607eb4f67a8aac
ajassi9751 <ajassi9751@gmail.com>
2026-02-06 19:31:49 +0000

    Added comments to scripts

--- setup-hooks.bat
+++ setup-hooks.bat
@@ -1,1 +1,4 @@
+@rem This can be invoked using .\setup-hooks or .\setup-hooks.bat because of how windows handles filenames for scripts
+@rem This recursivley copies files from .hooks to .git\hooks
+@rem /s and /e copies files and subfolders (there aren't any subfolders) recursivley and /Y gets rid of the confirmation prompt
 xcopy .\.hooks .\.git\hooks\ /s /e /Y
