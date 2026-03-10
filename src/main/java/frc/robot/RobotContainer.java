7ecf83e0adedd7c31911d64f4f3af24155e2eda5
OmarKarim747 <superoctopus747@gmail.com>
2026-03-10 16:54:32 -0400

    Made it not do double deadband on manual arm

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -304,7 +304,7 @@ public class RobotContainer {
                 Driver2.povDown().onTrue(Commands.run(()->intake.intakeArmDown(),intake));
                 Driver2.povUp().onTrue(Commands.run(()->intake.intakeArmUp(),intake));
                 Driver2.rightBumper().whileTrue(new RunCommand(() -> {
-                        intake.setArm(MathUtil.applyDeadband(Driver2.getLeftY(), Operator.kDriveDeadband+.02) * Constants.Intake.kINTAKE_ARM_MOTOR_SPEED);
+                        intake.setArm(MathUtil.applyDeadband(Driver2.getLeftY(), Operator.kDriveDeadband) * Constants.Intake.kINTAKE_ARM_MOTOR_SPEED);
                 //        climber.setClimber(MathUtil.applyDeadband(Driver2.getRightY(), Operator.kDriveDeadband) * Constants.Climber.kCLIMBER_MOTOR_SPEED);
                 }, intake));//, climber));
                 // Driver2.b().whileTrue(new RunCommand(() -> {
