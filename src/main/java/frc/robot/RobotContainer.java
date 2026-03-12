3bf0cdb9d3e64643d25e68881dac13b9f46f99ed
Sidh Parikh <sidhparikh@gmail.com>
2026-03-12 19:03:13 -0400

    hi

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -103,9 +103,9 @@ public class RobotContainer {
         public int listIndex = 0;
         private Boolean lastActiveAlliance = true;
         public double targetRPM = 1000;
-        private final StructArrayPublisher<Pose3d> fuelPublisher = NetworkTableInstance.getDefault()
-        .getStructArrayTopic("SmartDashboard/Vision/DetectedFuel", Pose3d.struct)
-        .publish();
+        // private final StructArrayPublisher<Pose3d> fuelPublisher = NetworkTableInstance.getDefault()
+        // .getStructArrayTopic("SmartDashboard/Vision/DetectedFuel", Pose3d.struct)
+        // .publish();
 
         private PathPlannerPath pathLeftToNeutral;
         private PathPlannerPath pathNeutralToLeft;
@@ -360,7 +360,7 @@ public class RobotContainer {
                 SmartDashboard.putNumber(autoName, listIndex);
                 SmartDashboard.putNumber("Shooter/Set RPM (In RobotContainer)",targetRPM);
                 SmartDashboard.putNumber("Drivetrain/Angular Velocity Error (dps)", drivetrain.getPigeon2().getAngularVelocityZDevice().getValueAsDouble());
-                fuelPublisher.set(photonVision.getFieldFuelPoses(drivetrain.getPose()).toArray(new Pose3d[0]));
+                // fuelPublisher.set(photonVision.getFieldFuelPoses(drivetrain.getPose()).toArray(new Pose3d[0]));
 
                 Pose2d currentPose = drivetrain.getPose();
                 
