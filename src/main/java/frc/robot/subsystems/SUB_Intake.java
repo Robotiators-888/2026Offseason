c2a6d975ded3355d90ff5b6a945b795327efb699
Sidh Parikh <sidhparikh@gmail.com>
2026-03-14 14:44:26 -0400

    [Auto] Made intake arm bypass stickdown in auto command

--- src/main/java/frc/robot/subsystems/SUB_Intake.java
+++ src/main/java/frc/robot/subsystems/SUB_Intake.java
@@ -154,7 +154,7 @@ public class SUB_Intake extends SubsystemBase {
             // intakeArmDown();
             setArm(-.50);
         } else {
-            setArm(-.1);
+            arm.set(-.1);
             intakeArmAndRollersUntil = true;
         }
     }
