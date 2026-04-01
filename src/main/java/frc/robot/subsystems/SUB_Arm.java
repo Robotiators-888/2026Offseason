468c223276886b0e273a53402122ae8c7102b95e
Aikam Jassi <sidhparikh@gmail.com>
2026-04-01 10:49:23 -0400

    Changes

--- src/main/java/frc/robot/subsystems/SUB_Arm.java
+++ src/main/java/frc/robot/subsystems/SUB_Arm.java
@@ -146,7 +146,7 @@ public class SUB_Arm extends SubsystemBase {
 
     /** Manual test drive for the arm */
     public void intakeArmTest() {
-        arm.set(-.3);
+        arm.set(-.5);
     }
 
     /** Drives the arm to the top setpoint using PID */
