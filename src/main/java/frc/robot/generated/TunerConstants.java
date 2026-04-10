ee0cbcb33ed22ac13de1b6702a4a1d865e8663c5
Sidh Parikh <sidhparikh@gmail.com>
2026-04-10 12:16:02 -0400

    Change drive motor current limit

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -60,7 +60,8 @@ public class TunerConstants {
             new CurrentLimitsConfigs()
                 .withStatorCurrentLimit(Amps.of(90)) //Calculated Slip Current
                 .withStatorCurrentLimitEnable(true)
-                .withSupplyCurrentLimit(Amps.of(45)) // Mr. Lange approved change from 50 -> 45 (consideration of 40 amp reccomendation)
+                // Mr Gerstener said to change from 45 -> 40
+                .withSupplyCurrentLimit(Amps.of(40)) // Mr. Lange approved change from 50 -> 45 (consideration of 40 amp reccomendation)
                 .withSupplyCurrentLimitEnable(true)
         );
     private static final TalonFXConfiguration steerInitialConfigs = new TalonFXConfiguration()
